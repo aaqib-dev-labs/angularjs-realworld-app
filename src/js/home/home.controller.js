2a657a7365de20ed68ae384afcefc55871a6c045
AaqibhafeezKhan <aaqibhafeezkhan@gmail.com>
2026-04-18 01:43:38 +0530

    Enhance: Fix primary API endpoint and integrate Space News API for improved functionality

--- src/js/home/home.controller.js
+++ src/js/home/home.controller.js
@@ -1,5 +1,5 @@
 class HomeCtrl {
-  constructor(User, Tags, AppConstants, $scope) {
+  constructor(User, Tags, AppConstants, $scope, $http) {
     'ngInject';
 
     this.appName = AppConstants.appName;
@@ -14,6 +14,12 @@ class HomeCtrl {
           this.tags = tags
         }
       );
+
+    // Fetch Space News as a bonus open-source API feature
+    $http.get('https://api.spaceflightnewsapi.net/v4/articles/?limit=3')
+      .then((res) => {
+        this.spaceNews = res.data.results;
+      });
 
     // Set current list to either feed or all, depending on auth status.
     this.listConfig = {
